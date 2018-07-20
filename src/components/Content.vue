419a2f6422a80d3b013c02585db95bcb064e7b79
heyuanqing1 <heyuanqing@jd.com>
2018-07-20 16:37:07 +0800

    实现简单基础功能

---
+++ src/components/Content.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <input type="text" v-on:keyup.enter="addTask" v-model="newTask"/>
+    <button v-on:click="addTask">确认</button>
+    <div>
+      <ul>
+        <li v-for="task in tasks" v-bind:class={finished:task.isFinished} v-on:click="finish(task)">{{task.content}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Content',
+  data() {
+    return {
+      tasks: [
+        {
+          content: "read books",
+         isFinished: false
+        }
+      ],
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({content: this.newTask, isFinished: false});
+      this.newTask = '';
+    },
+    finish(task) {
+
+      task.isFinished = !task.isFinished;
+    }
+  }
+}
+</script>
+<style>
+ul{
+  list-style: circle;
+  text-align: left;
+  width: 400px;
+  margin: 50px auto 0 auto;
+}
+.finished{
+  text-decoration: line-through;
+}
+</style>
